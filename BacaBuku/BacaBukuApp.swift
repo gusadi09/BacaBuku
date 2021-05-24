a5ad2228072776ad8caa846080470cdaef787635
gusadi09 <gusadi872@gmail.com>
2021-05-24 11:07:41 +0800

    Initial Commit

---
+++ BacaBuku/BacaBukuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BacaBukuApp.swift
+//  BacaBuku
+//
+//  Created by Gus Adi on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BacaBukuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BacaBuku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BacaBuku
+//
+//  Created by Gus Adi on 24/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
